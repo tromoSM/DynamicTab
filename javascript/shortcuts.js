13400d8cc8e1745b9612cf64c1ef0fb8c879d4c7
tromoSM <tromosm7@gmail.com>
2025-11-07 02:12:11 +0530

    javascript/shortcuts.js
    Added keyboard shortcuts for important tasks.

---
+++ javascript/shortcuts.js
@@ -0,0 +1,12 @@
+window.addEventListener(`DOMContentLoaded`,function(){
+    window.addEventListener(`keydown`,function(yo){
+      if(yo.ctrlKey&&yo.key=='d'){
+        searchEng$$main()
+        yo.preventDefault()
+      }
+       else if(yo.keyCode === 191 && !yo.shiftKey){
+        yo.preventDefault()
+        document.querySelector(`[tromosm="search-in"]`).focus()
+      }
+    })
+})
