af67ff9673387936cdf0fc2dd385bc3d521a7ceb
tromoSM <tromosm7@gmail.com>
2025-11-07 19:08:06 +0530

    javascript/pref-toolbar.js

--- javascript/pref-toolbar.js
+++ javascript/pref-toolbar.js
@@ -2,6 +2,7 @@ pref$$setting=['change wallpaper','change theme']
 window.addEventListener('DOMContentLoaded',function(){
 prefTool$$1=document.createElement(`div`)
 prefTool$$3=document.createElement(`div`)
+prefTool$$3.setAttribute(`ytsd`,`gg`)
 prefTool$$2=document.createElement(`img`)
 prefTool$$1.setAttribute('tromoSM',`bttn-0-pref`)
 document.body.appendChild(prefTool$$1)
@@ -10,6 +11,10 @@ prefTool$$1.appendChild(prefTool$$2)
 prefTool$$2.src='visual/UI/icons/settings.svg'
 prefTool$$2.setAttribute('tromoSM',`img-0-pref`)
 prefTool$$3.setAttribute('tromoSM',`dv-0-full`)
+close$$$toolpref=document.createElement(`button`)
+document.body.appendChild(close$$$toolpref)
+close$$$toolpref.setAttribute(`tromoSM`,`close$preff`)
+close$$$toolpref.innerHTML="×"
 pref$$setting.forEach(yo=>{
 b=document.createElement(`button`)
 irb=document.createElement(`button`)
@@ -20,10 +25,8 @@ if(yo=="change wallpaper"||yo=="change theme"){
 }
 prefTool$$1.addEventListener('click',function(){
 prefTool$$3.style.display="flex"
+close$$$toolpref.style.display="flex"
 })
-function close$$toolpref(){
-prefTool$$3.style.display="none"
-}
 p=document.createElement(`p`)
 g.src=`visual/UI/icons/pref-${yo.replace(" ","-")}.svg`
 p.textContent=yo
@@ -32,10 +35,17 @@ prefTool$$3.appendChild(b)
 b.appendChild(g)
 b.appendChild(p)
 b.appendChild(irb)
+})
+
+function close$$toolpref(){
+document.querySelector(`[ytsd=gg]`).style.display="none"
+}
+
 window.addEventListener("keydown",function(ahh){
 if(ahh.key=="Escape"){
     close$$toolpref()
 }
 })
-})
+close$$$toolpref.addEventListener("click",close$$toolpref)
+close$$$toolpref.stopPropagation()
 })
