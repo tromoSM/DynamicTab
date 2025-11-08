44154013e0ea37a0acce8308504efaee0459de74
tromoSM <tromosm7@gmail.com>
2025-11-08 15:25:59 +0530

    javascript/preferences.js

--- javascript/preferences.js
+++ javascript/preferences.js
@@ -1,6 +1,6 @@
 window.addEventListener(`DOMContentLoaded`,function(){
  document.querySelectorAll(`[action]`).forEach(all=>{
-  a=all.getAttribute(`action`)
+ let a=all.getAttribute(`action`)
   if(a=="file-$-wallpaper"){
  
   let g =document.createElement('input')
@@ -13,9 +13,9 @@ window.addEventListener(`DOMContentLoaded`,function(){
    g.click()  
   }) 
   g.addEventListener('change',function(){
-    fuhmetwin=this.files[0]
-    if(fuhmetwin.size>4*1024*1024){
-        alert(`File must be under 3.7MB. Might fail to continue.\nCurrent file size : ${parseInt(fuhmetwin.size/(1024*1024))}MB`)
+    let fuhmetwin=this.files[0]
+    if(fuhmetwin.size>3*1024*1024){
+        alert(`File must be under 3MB. Might fail to continue.\nCurrent file size : ${parseInt(fuhmetwin.size/(1024*1024))}MB`)
     }
     if(!fuhmetwin) return;
     const gurtahh= new FileReader()
@@ -26,7 +26,7 @@ window.addEventListener(`DOMContentLoaded`,function(){
             ol.remove()
         })
    if(fuhmetwin.type.startsWith("image/")){
-    main=document.createElement("img")
+   let  main=document.createElement("img")
     main.setAttribute("tromoSM",`background-main`)
     main.src=imim
     document.body.appendChild(main)
@@ -34,7 +34,7 @@ window.addEventListener(`DOMContentLoaded`,function(){
    }
    else if(fuhmetwin.type.startsWith("video/")){
     main=document.createElement("video")
-    appendedsource=document.createElement("source")
+    let appendedsource=document.createElement("source")
     main.setAttribute("tromoSM",`background-main`)
     main.setAttribute("autoplay",``)
     main.setAttribute("muted",``)
@@ -49,5 +49,42 @@ window.addEventListener(`DOMContentLoaded`,function(){
 
   })
 }
+ else if(a=="file-$-favicon"){
+ 
+    
+  let favin =document.createElement('input')
+  favin.setAttribute('type','file')
+  favin.setAttribute('hidden',"")
+  favin.setAttribute('tromoSM-action','file0favicon0')
+  favin.setAttribute("accept",'image/*')
+  document.body.appendChild(favin)
+  document.querySelector(`[action='file-$-favicon']`).addEventListener("click",function(){
+   favin.click()  
+  }) 
+  favin.addEventListener('change',function(){
+    let gurtbelike=this.files[0]
+    if(gurtbelike.size>(1024*1024)/2){
+        alert(`File must be under 0.5MB. Might fail to continue.\nCurrent file size : ${parseInt(gurtbelike.size/(1024*1024))}MB`)
+    }
+    if(!gurtbelike) return;
+    const gurtahh= new FileReader()
+    gurtahh.onload=()=>{
+        const imim=gurtahh.result
+        localStorage.setItem("imim_data_favicon",imim)
+        document.querySelectorAll(`[rel=icon]`).forEach(ol=>{
+            ol.remove()
+        })
+   if(gurtbelike.type.startsWith("image/")){
+   let  main=document.createElement("link")
+    main.setAttribute("rel",`icon`)
+    main.setAttribute('href',imim)
+    document.head.appendChild(main)
+   }
+   }
+    gurtahh.readAsDataURL(gurtbelike)
+
+  })
+    
+ }
  })
 })
